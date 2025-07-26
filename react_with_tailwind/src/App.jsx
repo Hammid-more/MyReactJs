d6cd5ca531e2e21b8bc8e26c83ebf23f42585f2d
Hammid-more <lawhammed45@gmail.com>
2025-07-26 12:08:12 +0100

    react_with_tailwind

---
+++ react_with_tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <div>
+      <p className='bg-black-700'>  Hello World</p>
+    </div>
+  )
+}
+
+export default App
+
+
