724d2d5c2fbb6561ad092e46e5a50b1de66726d0
Hammid-more <lawhammed45@gmail.com>
2025-08-02 17:38:15 +0100

    react_with_tailwind

--- react_with_tailwind/src/App.jsx
+++ react_with_tailwind/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
+import './App.css'
 
+function App(){
 
-function App() {
-  const [count, setCount] = useState(0)
+  const[count, setCount]= useState(0)
 
+  return(
+    
+    <div className="min-h-screen flex items-center justify-center bg-gray-200 p-4">
+ 
+        <h1 className="text-blue-500 text-3xl font-bold mb-2 justify-center ">
+          Lawal Hammed Olayemi
+        </h1>
 
-  return (
-    <div>
-      <p className='bg-black-700'>  Hello World</p>
-    </div>
-  )
+        <h2 className="text-green-600 text-2xl font-bold mb-2">
+          Frontend Developer
+        </h2>
+
+        <p className="text-gray-500 text-base font-normal">
+          I am a Software Developer, currently undergoing intensive training in modern web development, with a strong focus on React and Tailwind CSS to build real-world, responsive user interfaces.
+        </p>
+      </div>
+  );
 }
 
-export default App
+export default App;
 
 
